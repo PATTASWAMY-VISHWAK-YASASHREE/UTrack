a1914eacbd5fec657523f280ff9c4bbe499dac79
vara-prasad-07 <varaprasad2684@gmail.com>
2025-07-26 18:16:37 +0530

    update

--- src/pages/You.jsx
+++ src/pages/You.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import BottomNav from '../components/BottomNav';
 import './PageStyles.css';
+import {auth} from '../firebase'
+import { signOut} from 'firebase/auth';
+import LogoutButton from '../components/LogoutButton';
 
 const You = () => {
+  const handleLogout = () => signOut(auth);
   return (
     <div className="page you-page">
       <div className="header">You</div>
       <div className="placeholder-center">Profile and settings coming soon.</div>
+      <div className="flex justify-end p-4">
+      <LogoutButton onLogout={handleLogout} />
+    </div>
       <BottomNav />
     </div>
   );
